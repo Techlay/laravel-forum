09ba22074a4fa1240e39c3b80df5dc954c9f9261
Techlay <lytechlay@gmail.com>
2018-10-14 15:02:41 +1100

    Meta information and pagination

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,8 +1,12 @@
-<div class="card-header">
-    <a href="#">
-        {{ $reply->owner->name }}
-    </a> said {{ $reply->created_at->diffForHumans() }}...
-</div>
-<div class="card-body">
-    {{ $reply->body }}
+<div class="card">
+    <div class="card-header">
+        <a href="#">
+            {{ $reply->owner->name }}
+        </a> said {{ $reply->created_at->diffForHumans() }}...
+    </div>
+    <div class="card-body">
+        {{ $reply->body }}
+    </div>
+
+
 </div>
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,52 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">
-                <a href="#">{{ $thread->creator->name }}</a> posted:
-                    {{ $thread->title }}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="card">
+                    <h5 class="card-header">
+                        <a href="">{{ $thread->creator->name }}</a> posted:
+                        {{ $thread->title }}
+                    </h5>
+
+                    <div class="card-body">
+                        {{ $thread->body }}
+                    </div>
                 </div>
 
-                <div class="card-body">
-                    {{ $thread->body }}
-                </div>
-
-            </div>
-        </div>
-    </div>
-
-    <br>
-
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                @foreach ($thread->replies as $reply)
+                @foreach($replies as $reply)
+                    <br>
                     @include('threads.reply')
                 @endforeach
+
+                <br>
+                {{ $replies->links() }}
+
+                @if(auth()->check())
+                    <br>
+                    <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <textarea name="body" id="body" rows="5" class="form-control"
+                                      placeholder="Have something to say?"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Post</button>
+                    </form>
+                @else
+                    <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this
+                        discussion.</p>
+                @endif
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        This thread was published {{ $thread->created_at->diffForHumans() }} by
+                        <a href="#">{{ $thread->creator->name }}</a>, and currently
+                        has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}.
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-    <br>
-    @if (auth()->check())
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <form method="POST" action="{{ $thread->path() . '/replies' }}">
-                    {{ csrf_field() }}
-                    
-                    <div class="form-group">
-                        <textarea name="body" id="body" class="form-control" placeholder="Have something to say" rows="5"></textarea>
-                    </div>
-                    
-                    <button type="submit" class="btn btn-primary">Post</button>
-                </form>
-            </div>
-        </div>
-    @else
-        <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
-    @endif
-
-</div>
 @endsection
