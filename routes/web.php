b43471660b6d1962da85d87bdcbb26301e28d774
Techlay <lytechlay@gmail.com>
2018-10-15 10:58:24 +1100

    A user has a profile

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,5 @@ Route::post('threads', 'ThreadController@store');
 Route::get('threads/{channel}', 'ThreadController@index');
 Route::post('/threads/{channel}/{thread}/replies', 'ReplyController@store');
 Route::post('/replies/{reply}/favorites', 'FavoriteController@store');
+
+Route::get('/profiles/{user}', 'ProfileController@show')->name('profile');
