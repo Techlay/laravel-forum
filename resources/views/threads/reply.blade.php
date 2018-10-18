e52591f7dc3be6b68f5c8b42d536329165e4b676
Techlay <lytechlay@gmail.com>
2018-10-18 23:49:27 +1100

    How to construct an activity feed with TDD

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,11 +1,11 @@
 <div class="card">
     <div class="card-header">
         <div class="level">
-            <h5 class="flex">
+            <div class="flex">
                 <a href="{{ route('profile', $reply->owner) }}">
                     {{ $reply->owner->name }}
                 </a> said {{ $reply->created_at->diffForHumans() }}...
-            </h5>
+            </div>
             <div>
                 <form method="POST" action="/replies/{{ $reply->id }}/favorites">
                     {{ csrf_field() }}
