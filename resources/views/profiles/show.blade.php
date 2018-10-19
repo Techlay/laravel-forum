94a0e91c6308783991f0211772a16dc02c6dbea3
Techlay <lytechlay@gmail.com>
2018-10-19 17:07:18 +1100

    How to construct an activity feed with TDD part 2

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -9,25 +9,14 @@
                     <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
                 </h1>
                 <hr>
-                @foreach($threads as $thread)
-                    <div class="card">
-                        <div class="card-header">
-                            <div class="level">
-                                <span class="flex">
-                                    <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted:
-                                    <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
-                                </span>
-                                <span>{{ $thread->created_at->diffForHumans() }}</span>
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            {{ $thread->body }}
-                        </div>
-                    </div>
-                    <br>
+                @foreach($activities as $date => $activity)
+                    <h3>{{ $date }}</h3>
+                    @foreach($activity as $record)
+                        @include("profiles.activities.{$record->type}", ['activity' => $record])
+                    @endforeach
                 @endforeach
                 <br>
-                {{ $threads->links() }}
+{{--                {{ $threads->links() }}--}}
             </div>
         </div>
     </div>
