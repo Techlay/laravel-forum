37e48ab340c8141a2da6bb7446713a5654f8a6fe
Techlay <lytechlay@gmail.com>
2018-10-22 22:23:09 +1100

    A Vue reply component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,10 @@
         .flex {
             flex: 1;
         }
+
+        [v-cloak] {
+            display: none;
+        }
     </style>
 </head>
 
--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,34 +1,46 @@
-<div id="reply-{{ $reply->id }}" class="card">
-    <div class="card-header">
-        <div class="level">
-            <div class="flex">
-                <a href="{{ route('profile', $reply->owner) }}">
-                    {{ $reply->owner->name }}
-                </a> said {{ $reply->created_at->diffForHumans() }}...
-            </div>
-            <div>
-                <form method="POST" action="/replies/{{ $reply->id }}/favorites">
-                    {{ csrf_field() }}
+<reply :attributes="{{ $reply }}" inline-template v-cloak>
+    <div id="reply-{{ $reply->id }}" class="card">
+        <div class="card-header">
+            <div class="level">
+                <div class="flex">
+                    <a href="{{ route('profile', $reply->owner) }}">
+                        {{ $reply->owner->name }}
+                    </a> said {{ $reply->created_at->diffForHumans() }}...
+                </div>
+                <div>
+                    <form method="POST" action="/replies/{{ $reply->id }}/favorites">
+                        {{ csrf_field() }}
 
-                    <button type="submit" class="btn btn-outline-primary" {{ $reply->isFavorited() ? 'disabled' : '' }}>
-                        {{ $reply->favorites_count }} {{ str_plural('Favorite', $reply->favorites_count) }}
-                    </button>
+                        <button type="submit"
+                                class="btn btn-outline-primary" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                            {{ $reply->favorites_count }} {{ str_plural('Favorite', $reply->favorites_count) }}
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            <div v-if="editing">
+                <div class="form-group">
+                    <textarea class="form-control" v-model="body"></textarea>
+                </div>
+
+                <button class="btn btn-sm btn-primary" @click="update">Update</button>
+                <button class="btn btn-sm btn-link" @click="editing = false">Cancel</button>
+            </div>
+
+            <div v-else v-text="body"></div>
+        </div>
+
+        @can('update', $reply)
+            <div class="card-footer level">
+                <button class="btn btn-sm mr-2" @click="editing = true">Edit</button>
+                <form method="POST" action="/replies/{{ $reply->id }}">
+                    {{ csrf_field() }} {{ method_field('DELETE') }}
+
+                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
                 </form>
             </div>
-        </div>
+        @endcan
     </div>
-    <div class="card-body">
-        {{ $reply->body }}
-    </div>
-
-    @can('update', $reply)
-        <div class="card-footer">
-            <form method="POST" action="/replies/{{ $reply->id }}">
-                {{ csrf_field() }}
-                {{ method_field('DELETE') }}
-
-                <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-            </form>
-        </div>
-    @endcan
-</div>
+</reply>
