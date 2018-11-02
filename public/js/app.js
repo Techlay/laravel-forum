db4b4bd4044297e70ae3410e12b00f02c9b712f5
Techlay <lytechlay@gmail.com>
2018-11-02 15:30:22 +1100

    A user can filter by unanswered threads

--- public/js/app.js
+++ public/js/app.js
@@ -64563,6 +64563,8 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
             this.dataSet = data;
             this.items = data.data;
+
+            window.scrollTo(0, 0);
         }
     }
 });
