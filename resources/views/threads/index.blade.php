e2633f924fc45337bcdf2fb3a377bcad3f04634d
Techlay <lytechlay@gmail.com>
2018-11-07 11:51:25 +1100

    Basic UI tweaks

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -3,38 +3,9 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            @forelse ($threads as $thread)
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">
-                            <div class="level">
-                                <div class="flex">
-                                    <a href="{{ $thread->path() }}">
-                                        @if(auth()->check() && $thread->hasUpdatesFor(auth()->user()))
-                                            <strong>
-                                                {{ $thread->title }}
-                                            </strong>
-                                        @else
-                                            {{ $thread->title }}
-                                        @endif
-                                    </a>
-                                </div>
-                                <a href="{{ $thread->path() }}">
-                                    {{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}
-                                </a>
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            <div class="body">{{ $thread->body }}</div>
-                        </div>
-                    </div>
-                    <br>
-                </div>
-            @empty
-                <p>There are no relevant results at this time.</p>
-            @endforelse
+            @include('threads._list')
 
-
+            {{ $threads->render() }}
         </div>
     </div>
 @endsection
