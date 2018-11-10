a49be5826ede1aa6c376cdd0a039e7c3b60a3f93
Techlay <lytechlay@gmail.com>
2018-11-10 21:30:56 +1100

    An administrator may lock any thread part 3

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,61 +1,55 @@
-@extends('layouts.app')
-
+@extends('layouts.app') 
 @section('header')
-    <link rel="stylesheet" href="/css/vendor/jquery.atwho.css">
+<link rel="stylesheet" href="/css/vendor/jquery.atwho.css">
 @endsection
-
+ 
 @section('content')
-    <thread-view :initial-replies-count="{{ $thread->replies_count }}" inline-template>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">
-                            <div class="level">
-                                <img src="{{ $thread->creator->avatar_path }}" alt="{{ $thread->creator->name }}"
-                                     width="25" height="25" class="mr-2">
-                                <div class="flex">
-                                    <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a>
-                                    posted:
-                                    {{ $thread->title }}
-                                </div>
-                                @can('update', $thread)
-                                    <form action="{{ $thread->path() }}" method="POST">
-                                        {{ csrf_field() }}
-                                        {{ method_field('DELETE') }}
-
-                                        <button type="submit" class="btn btn-link">Delete Thread</button>
-                                    </form>
-                                @endcan
+<thread-view :data-replies-count="{{ $thread->replies_count }}" :data-locked="{{ $thread->locked }}" inline-template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="level">
+                            <img src="{{ $thread->creator->avatar_path }}" alt="{{ $thread->creator->name }}" width="25" height="25" class="mr-2">
+                            <div class="flex">
+                                <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted:
+                                {{ $thread->title }}
                             </div>
+                            @can('update', $thread)
+                            <form action="{{ $thread->path() }}" method="POST">
+                                {{ csrf_field() }} {{ method_field('DELETE') }}
 
+                                <button type="submit" class="btn btn-link">Delete Thread</button>
+                            </form>
+                            @endcan
                         </div>
 
-                        <div class="card-body">
-                            {{ $thread->body }}
-                        </div>
                     </div>
-                    <br>
 
-                    <replies @added="repliesCount++" @removed="repliesCount--"></replies>
+                    <div class="card-body">
+                        {{ $thread->body }}
+                    </div>
                 </div>
-                <div class="col-md-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <p>This thread was published {{ $thread->created_at->diffForHumans() }} by
-                                <a href="#">{{ $thread->creator->name }}</a>, and currently
-                                has <span
-                                        v-text="repliesCount"></span> {{ str_plural('comment', $thread->replies_count) }}
-                                .
-                            </p>
-                            <p>
-                                <subscribe-button
-                                        :active="{{ json_encode($thread->isSubscribedTo) }}"></subscribe-button>
-                            </p>
-                        </div>
+                <br>
+
+                <replies @added="repliesCount++" @removed="repliesCount--"></replies>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <p>This thread was published {{ $thread->created_at->diffForHumans() }} by
+                            <a href="#">{{ $thread->creator->name }}</a>, and currently has <span v-text="repliesCount"></span>                            {{ str_plural('comment', $thread->replies_count) }} .
+                        </p>
+                        <p>
+                            <subscribe-button :active="{{ json_encode($thread->isSubscribedTo) }}" v-if="signedIn"></subscribe-button>
+
+                            <button class="btn btn-outline-primary" v-if="authorize('isAdmin') && !locked" @click="locked = true">Lock</button>
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
-    </thread-view>
+    </div>
+</thread-view>
 @endsection
