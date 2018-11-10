e595dc7bbc0b7d26b9919988478b49b0aa3f4fb6
Techlay <lytechlay@gmail.com>
2018-11-10 15:48:33 +1100

    Confusing errors and solutions

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,19 @@
 
 namespace Tests;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        DB::statement('PRAGMA foreign_keys=on;');
+    }
 
     protected function signIn($user = null)
     {
