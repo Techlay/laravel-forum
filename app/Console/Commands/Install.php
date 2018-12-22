c3ab2329083f4614300026bf9a1581450f4e51b7
Techlay <lytechlay@gmail.com>
2018-12-22 13:10:33 +1100

    Cleaner environment file update method

--- app/Console/Commands/Install.php
+++ app/Console/Commands/Install.php
@@ -42,58 +42,39 @@ class Install extends Command
         if (!file_exists('.env')) {
             exec('mv .env.example .env');
             $this->line("\r\n.env file successfully created\r\n");
-
-            exec('composer install');
-            $this->line("\r\n ~ App dependencies successfully installed\r\n");
-
-            exec('php artisan key:generate');
-            $this->line("\r\n ~ Secret key properly generated\r\n");
-
-            // Handle different db types (SQLite etc..), currently defaults to MySQL
-
-            $dbEnv['DB_DATABASE'] = $this->ask('Database name');
-            $dbEnv['DB_USERNAME'] = $this->ask('Database user');
-            $dbEnv['DB_PASSWORD'] = $this->ask('Database password ("null" for no password)');
-
-            $this->changeEnv($dbEnv);
-
-            // Check if the DB connection is actually working or not
-
-            if ($this->confirm('Do you want to automatically setup the database tables?', true)) {
-                exec('php artisan migrate');
-                $this->line("\r\n~ Database successfully migrated\r\n");
-            }
-
-            // Master user creation (might be useful in case of future ACL implementation)
-
-            $this->outro();
         }
+
+        if (strlen(config('app.key')) === 0) {
+            $this->call('key:generate');
+            $this->line("\r\n ~ Secret key properly generated\r\n");
+        }
+
+        $dbEnv['DB_DATABASE'] = $this->ask('Database name');
+        $dbEnv['DB_USERNAME'] = $this->ask('Database user');
+        $dbEnv['DB_PASSWORD'] = $this->ask('Database password ("null" for no password)');
+
+        $this->updateEnvironmentFile($dbEnv);
+
+        // Check if the DB connection is actually working or not
+
+        if ($this->confirm('Do you want to automatically setup the database tables?', true)) {
+            exec('migrate');
+            $this->line("\r\n~ Database successfully migrated\r\n");
+        }
+
+        // Master user creation (might be useful in case of future ACL implementation)
+
+        $this->outro();
     }
 
-    protected function changeEnv($data = array())
+    protected function updateEnvironmentFile($updatedValues)
     {
-        if (count($data) > 0) {
-            $env = file_get_contents(base_path() . '/.env');
-            $env = preg_split('/\s+/', $env);
-
-            foreach ((array)$data as $key => $value) {
-                foreach ($env as $env_key => $env_value) {
-                    $entry = explode("=", $env_value, 2);
-
-                    if ($entry[0] == $key) {
-                        $env[$env_key] = $key . "=" . $value;
-                    } else {
-                        $env[$env_key] = $env_value;
-                    }
-                }
-            }
-
-            $env = implode("\n", $env);
-            file_put_contents(base_path() . '/.env', $env);
-
-            return true;
-        } else {
-            return false;
+        foreach ($updatedValues as $key => $value) {
+            file_put_contents($this->laravel->environmentFilePath(), preg_replace(
+                "/{$key}={.*}",
+                $key.'='.$value,
+                file_get_contents($this->laravel->environmentFilePath())
+            ));
         }
     }
 
@@ -104,6 +85,6 @@ class Install extends Command
 
     protected function outro()
     {
-        $this->info("\r\n>> The intallation process is complete. Enjoy your new forum! <<\r\n");
+        $this->info("\r\n>> The installation process is complete. Enjoy your new forum! <<\r\n");
     }
 }
