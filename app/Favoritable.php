1995cbd87db9b9ef22e9e347283beb469b1b1357
Techlay <lytechlay@gmail.com>
2018-12-22 13:19:30 +1100

    Fix awarded user

--- app/Favoritable.php
+++ app/Favoritable.php
@@ -22,8 +22,6 @@ trait Favoritable
         $attributes = ['user_id' => auth()->id()];
 
         if (!$this->favorites()->where($attributes)->exists()) {
-            Reputation::award(auth()->user(), Reputation::REPLY_FAVORITED);
-
             return $this->favorites()->create($attributes);
         }
     }
@@ -33,8 +31,6 @@ trait Favoritable
         $attributes = ['user_id' => auth()->id()];
 
         $this->favorites()->where($attributes)->get()->each->delete();
-
-        Reputation::reduce(auth()->user(), Reputation::REPLY_FAVORITED);
     }
 
     public function isFavorited()
--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -3,23 +3,39 @@
 namespace App\Http\Controllers;
 
 use App\Reply;
+use App\Reputation;
 
 class FavoriteController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
+    /**
+     * Store a new favorite in the database.
+     *
+     * @param Reply $reply
+     */
     public function store(Reply $reply)
     {
         $reply->favorite();
 
-        return back();
+        Reputation::award($reply->owner, Reputation::REPLY_FAVORITED);
     }
 
+    /**
+     * Delete the favorite.
+     *
+     * @param Reply $reply
+     */
     public function destroy(Reply $reply)
     {
         $reply->unfavorite();
+
+        Reputation::reduce($reply->owner, Reputation::REPLY_FAVORITED);
     }
 }
