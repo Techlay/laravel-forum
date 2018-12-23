605eadc8ad5d6bbca6a15536b278170ba8a9edf8
Techlay <lytechlay@gmail.com>
2018-12-23 12:35:49 +1100

    wysiwgy cleanup

--- resources/js/components/NewReply.vue
+++ resources/js/components/NewReply.vue
@@ -2,8 +2,7 @@
     <div>
         <div v-if="signedIn">
             <div class="form-group">
-                <wysiwyg name="body" v-model="body" placeholder="Have something to say"
-                         :shouldClear="completed"></wysiwyg>
+                <wysiwyg name="body" v-model="body" placeholder="Have something to say"></wysiwyg>
             </div>
 
             <button type="submit" class="btn btn-primary" @click="addReply">Post</button>
@@ -21,8 +20,7 @@
     export default {
         data() {
             return {
-                body: '',
-                completed: false
+                body: ''
             }
         },
 
@@ -48,7 +46,6 @@
                     })
                     .then(({data}) => {
                         this.body = '';
-                        this.completed = true;
 
                         flash('Your reply has been posted.');
 
