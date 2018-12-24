5afab106560ec3b8fff8e970760d72d44496edad
Techlay <lytechlay@gmail.com>
2018-12-24 20:39:12 +1100

    Move method up

--- app/Reply.php
+++ app/Reply.php
@@ -9,12 +9,28 @@ class Reply extends Model
 {
     use Favoritable, RecordsActivity;
 
+    /**
+     * Don't auto-apply mass assignment protection.
+     *
+     * @var array
+     */
     protected $guarded = [];
 
+    /**
+     * The relations to eager load on every query.
+     * @var array
+     */
     protected $with = ['owner', 'favorites'];
 
+    /**
+     * The accessors to append to the model's array form.
+     * @var array
+     */
     protected $appends = ['isFavorited', 'isBest'];
 
+    /**
+     * Boot the reply instance.
+     */
     protected static function boot()
     {
         parent::boot();
@@ -32,21 +48,41 @@ class Reply extends Model
         });
     }
 
+    /**
+     * A reply has an owner.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function owner()
     {
         return $this->belongsTo(User::class, 'user_id');
     }
 
+    /**
+     * A reply belongs to a thread.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function thread()
     {
         return $this->belongsTo(Thread::class);
     }
 
+    /**
+     * Determined if the reply was just published a moment ago.
+     *
+     * @return bool
+     */
     public function wasJustPublished()
     {
         return $this->created_at->gt(Carbon::now()->subMinute());
     }
 
+    /**
+     * Fetch all mentioned users within the reply's body.
+     *
+     * @return array
+     */
     public function mentionedUsers()
     {
         preg_match_all('/@([\w\-]+)/', $this->body, $matches);
@@ -54,28 +90,58 @@ class Reply extends Model
         return $matches[1];
     }
 
+    /**
+     * Determined the path to the reply.
+     *
+     * @return string
+     */
     public function path()
     {
         return $this->thread->path() . "#reply-{$this->id}";
     }
 
-    public function setBodyAttribute($body)
+
+    /**
+     * Access the body attribute.
+     * @param string $body
+     * @return string
+     */
+    public function getBodyAttribute($body)
     {
-        $this->attributes['body'] = preg_replace('/@([\w\-]+)/', '<a href="/profiles/$1">$0</a>', $body);
+        return \Purify::clean($body);
     }
 
+    /**
+     * Set the body attribute.
+     *
+     * @param string $body
+     */
+    public function setBodyAttribute($body)
+    {
+        $this->attributes['body'] = preg_replace(
+            '/@([\w\-]+)/',
+            '<a href="/profiles/$1">$0</a>',
+            $body
+        );
+    }
+
+    /**
+     * Determine if the current reply is marked as the best.
+     *
+     * @return bool
+     */
     public function isBest()
     {
         return $this->thread->best_reply_id == $this->id;
     }
 
+    /**
+     * Determine if the current reply is marked as the best.
+     *
+     * @return bool
+     */
     public function getIsBestAttribute()
     {
         return $this->isBest();
-    }
-
-    public function getBodyAttribute($body)
-    {
-        return \Purify::clean($body);
     }
 }
