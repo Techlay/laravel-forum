14e20a6f2f96ea561f18f9cd8bc79e1a8d0a98d8
Techlay <lytechlay@gmail.com>
2018-12-25 20:52:11 +1100

    Rename computed property for filtered channels

--- resources/js/components/ChannelDropdown.vue
+++ resources/js/components/ChannelDropdown.vue
@@ -19,7 +19,7 @@
             </div>
 
             <ul class="list-group channel-list">
-                <li class="list-group-item" v-for="channel in filteredThreads">
+                <li class="list-group-item" v-for="channel in filteredChannels">
                     <a :href="`/threads/${channel.slug}`" v-text="channel.name"></a>
                 </li>
             </ul>
@@ -58,7 +58,7 @@
             }
         },
         computed: {
-            filteredThreads() {
+            filteredChannels() {
                 return this.channels.filter(channel => {
                     return channel.name.toLowerCase().startsWith(this.filter.toLocaleLowerCase())
                 })
