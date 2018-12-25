39ff04267869af1806dd8bd7fc68e331572f393c
Techlay <lytechlay@gmail.com>
2018-12-25 17:44:11 +1100

    Delete old avatar

--- app/Http/Controllers/Api/UserAvatarController.php
+++ app/Http/Controllers/Api/UserAvatarController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 
 class UserAvatarController extends Controller
 {
@@ -11,6 +12,8 @@ class UserAvatarController extends Controller
         request()->validate([
             'avatar' => ['required', 'image']
         ]);
+
+        Storage::disk('public')->delete(auth()->user()->getOriginal('avatar_path'));
 
         auth()->user()->update([
             'avatar_path' => request()->file('avatar')->store('avatars', 'public')
