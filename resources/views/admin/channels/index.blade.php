ec96e338ddcfe7aa18e47a674077a041fbbc1db1
Techlay <lytechlay@gmail.com>
2018-12-29 18:21:12 +1100

    Use more semantic class to indicate archived channels

--- resources/views/admin/channels/index.blade.php
+++ resources/views/admin/channels/index.blade.php
@@ -15,7 +15,7 @@
         </thread>
         <tbody>
         @forelse($channels as $channel)
-            <tr class="{{ $channel->archived ? 'table-info' : '' }}">
+            <tr class="{{ $channel->archived ? 'table-danger' : '' }}">
                 <td>{{ $channel->name }}</td>
                 <td>{{ $channel->slug }}</td>
                 <td>{{ $channel->description }}</td>
