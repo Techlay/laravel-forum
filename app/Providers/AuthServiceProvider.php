a7c48227eef8d0272380bbcacdfff34adffe7b81
Techlay <lytechlay@gmail.com>
2019-01-08 18:15:10 +1100

    Best reply award should be lost when reply is deleted

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -26,9 +25,5 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-//        \Gate::before(function ($user) {
-//            if ($user->name === 'John Doe') return true;
-//        });
     }
 }
